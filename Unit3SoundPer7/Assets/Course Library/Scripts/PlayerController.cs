805265b51e70ff1d763da967cdacb65b9a51ed9a
Clark Rodriguez Millan <740394@student.dusd.net>
2024-04-18 15:25:53 -0700

    at Diert splatter step 3
    at dirt splatter for sounds thing stopes at step 3

--- Unit3SoundPer7/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit3SoundPer7/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,27 +8,48 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+
+    public bool gameOver;
+
+    private Animator playerAnim;
+
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround=true;
+      
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
+            explosionParticle.Play();
+        }
     }
 
 }
--- Unit3SoundPer7/Assets/Course Library/Scripts/SpawnManager.cs
+++ Unit3SoundPer7/Assets/Course Library/Scripts/SpawnManager.cs
@@ -9,10 +9,14 @@ public class SpawnManager : MonoBehaviour
 
     private float startDelay = 2;
     private float repeatRate = 2;
+
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle" , startDelay , repeatRate);
+
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle ()
     {
-        Instantiate(obstcalePrefab, spawnPos, obstcalePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstcalePrefab, spawnPos, obstcalePrefab.transform.rotation);
+        }
     }
 }
